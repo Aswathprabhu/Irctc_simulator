62f0313937d91635499102d104105b7c05aa278c
Aswath <aswathprabhu.r@zohocorp.com>
2019-02-12 12:05:21 +0530

    Updates

---
+++ precache-manifest.cd1bc0e29cd039a934a428aa5934cb30.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc56a4013ca361f5e3df3bb2b525576a",
+    "url": "/Irctc_simulator/static/media/loading.cc56a401.svg"
+  },
+  {
+    "revision": "87f6920fa26840cacaff",
+    "url": "/Irctc_simulator/static/js/runtime~main.87f6920f.js"
+  },
+  {
+    "revision": "b46c12289963617dcbeb",
+    "url": "/Irctc_simulator/static/js/main.b46c1228.chunk.js"
+  },
+  {
+    "revision": "1ef6571cb33be0434bfe",
+    "url": "/Irctc_simulator/static/js/1.1ef6571c.chunk.js"
+  },
+  {
+    "revision": "b46c12289963617dcbeb",
+    "url": "/Irctc_simulator/static/css/main.51edfdd6.chunk.css"
+  },
+  {
+    "revision": "1ef6571cb33be0434bfe",
+    "url": "/Irctc_simulator/static/css/1.b3e7d705.chunk.css"
+  },
+  {
+    "revision": "e4df379d24ce763ba1bd1f999977a5d6",
+    "url": "/Irctc_simulator/index.html"
+  }
+];
